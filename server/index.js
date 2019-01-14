0f7e35e07bc14bbc6c33cdeafbf488abef3e0295
Dean Asadi <eng.de86@gmail.com>
2019-01-13 19:09:52 -0600

    deleted unused files

--- server/index.js
+++ server/index.js
@@ -2,10 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ res: "response was succesful..." });
+  res.send({ res: "response was succesful" });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`connected to port ${PORT} `);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
