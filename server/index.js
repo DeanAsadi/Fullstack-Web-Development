6d71dc70aaf8f58b9c6f989220347f4168bc3289
Dean Asadi <eng.de86@gmail.com>
2019-01-13 18:27:23 -0600

    deleting files

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ res: "response was succesful" });
+  res.send({ res: "response was succesful..." });
 });
 
 const PORT = process.env.PORT || 5000;
