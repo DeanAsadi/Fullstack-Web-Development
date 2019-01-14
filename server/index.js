cc33bb77b811cd116c9855d1045776f3ac01e65c
Dean Asadi <eng.de86@gmail.com>
2019-01-13 18:48:08 -0600

    fixing package.json issue

--- server/index.js
+++ server/index.js
@@ -5,5 +5,7 @@ app.get("/", (req, res) => {
   res.send({ res: "response was succesful..." });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`connected to port ${PORT} `);
+});
